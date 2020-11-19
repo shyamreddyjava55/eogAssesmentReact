8528893ec565c39513ae3d92c926188088cf9f42
shyam <shyam@example.com>
2020-11-20 02:30:54 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import createStore from './store';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import NowWhat from './components/NowWhat';
+import Dashboard from './components/Dashboard';
+import { render } from 'react-dom';
+
+const store = createStore();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(39,49,66)',
+    },
+    secondary: {
+      main: 'rgb(197,208,222)',
+    },
+    background: {
+      default: 'rgb(226,231,238)',
+    },
+  },
+});
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Dashboard />
+      </div>
+    )
+  }
+}
+
+export default App;
